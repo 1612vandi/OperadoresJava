1af381518ffd6143e6c57f24f01d415a084d1131
vandi1612 <vandidanielmartinsfrancisco@gmail.com>
2022-12-24 23:04:08 +0000

    Organizando

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Operadores Aritmaticos
+        // Operadores Unario
+        // Operadores Ternario
+        // Operadores Logicos
+        // Operadores Boolean
+    }
+}
