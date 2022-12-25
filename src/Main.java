2b8bc1130e08e990f4ea8f53b8dabd98f63aaaa1
Vandi Francisco <73638807+1612vandi@users.noreply.github.com>
2022-12-25 22:37:46 +0000

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Operadores Aritmaticos
-        // Operadores Unario
-        // Operadores Ternario
-        // Operadores Logicos
-        // Operadores Boolean
-    }
-}
